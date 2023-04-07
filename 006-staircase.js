640b73e87cd550d28fd02a41f4695e2dc333223e
Angelo Ambrósio <angelotosta1105@gmail.com>
2023-04-07 08:55:59 -0300

    Staircase exercise done

---
+++ 006-staircase.js
@@ -0,0 +1,24 @@
+function staircase(n) {
+    // Write your code here
+    let space = " "
+    let symbol = "#"
+
+    
+    let stair = ""
+    
+    for(let index = 0; index < n; index++){
+        let stairRow = ""
+        for(let indexColumn = 0; indexColumn < n; indexColumn++){
+            if(indexColumn < (n - index - 1)){
+                stairRow += space
+            } else {
+                stairRow += symbol
+            }
+        }
+        stairRow += "\n"        
+        stair += stairRow
+    }
+        console.log(stair)
+}
+
+staircase(4)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hackerrank-algorithms-exercises
+
+Here I'm saving all the attemps of solving HackerRank algorithms exercises. The goal is to improve my logic habilities.
